2460a9fc3b7fced146fd2a2c577d3b013dc44e0d
isnkng <35251708+isnkng@users.noreply.github.com>
2018-01-16 15:30:40 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -10,11 +10,154 @@ enum TropicalFruit {
     Coconut = 2
 }
 
+enum Note {
+    //% blockIdentity=music.noteFrequency enumval=262
+    C = 262,
+    //% block=C#
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F = 349,
+    //% block=F#
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G = 392,
+    //% block=G#
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B = 494,
+    //% blockIdentity=music.noteFrequency enumval=131
+    C3 = 131,
+    //% block=C#3
+    //% blockIdentity=music.noteFrequency enumval=139
+    CSharp3 = 139,
+    //% blockIdentity=music.noteFrequency enumval=147
+    D3 = 147,
+    //% blockIdentity=music.noteFrequency enumval=156
+    Eb3 = 156,
+    //% blockIdentity=music.noteFrequency enumval=165
+    E3 = 165,
+    //% blockIdentity=music.noteFrequency enumval=175
+    F3 = 175,
+    //% block=F#3
+    //% blockIdentity=music.noteFrequency enumval=185
+    FSharp3 = 185,
+    //% blockIdentity=music.noteFrequency enumval=196
+    G3 = 196,
+    //% block=G#3
+    //% blockIdentity=music.noteFrequency enumval=208
+    GSharp3 = 208,
+    //% blockIdentity=music.noteFrequency enumval=220
+    A3 = 220,
+    //% blockIdentity=music.noteFrequency enumval=233
+    Bb3 = 233,
+    //% blockIdentity=music.noteFrequency enumval=247
+    B3 = 247,
+    //% blockIdentity=music.noteFrequency enumval=262
+    C4 = 262,
+    //% block=C#4
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp4 = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D4 = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb4 = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E4 = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F4 = 349,
+    //% block=F#4
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp4 = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G4 = 392,
+    //% block=G#4
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp4 = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A4 = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb4 = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B4 = 494,
+    //% blockIdentity=music.noteFrequency enumval=523
+    C5 = 523,
+    //% block=C#5
+    //% blockIdentity=music.noteFrequency enumval=555
+    CSharp5 = 555,
+    //% blockIdentity=music.noteFrequency enumval=587
+    D5 = 587,
+    //% blockIdentity=music.noteFrequency enumval=622
+    Eb5 = 622,
+    //% blockIdentity=music.noteFrequency enumval=659
+    E5 = 659,
+    //% blockIdentity=music.noteFrequency enumval=698
+    F5 = 698,
+    //% block=F#5
+    //% blockIdentity=music.noteFrequency enumval=740
+    FSharp5 = 740,
+    //% blockIdentity=music.noteFrequency enumval=784
+    G5 = 784,
+    //% block=G#5
+    //% blockIdentity=music.noteFrequency enumval=831
+    GSharp5 = 831,
+    //% blockIdentity=music.noteFrequency enumval=880
+    A5 = 880,
+    //% blockIdentity=music.noteFrequency enumval=932
+    Bb5 = 932,
+    //% blockIdentity=music.noteFrequency enumval=988
+    B5 = 988,
+}
+
+enum BeatFraction {
+    //% block=1
+    Whole = 1,
+    //% block="1/2"
+    Half = 2,
+    //% block="1/4"
+    Quarter = 4,
+    //% block="1/8"
+    Eighth = 8,
+    //% block="1/16"
+    Sixteenth = 16,
+    //% block="2"
+    Double = 32,
+    //% block="4",
+    Breve = 64
+}
+
+enum MelodyOptions {
+    //% block="once""
+    Once = 1,
+    //% block="forever"
+    Forever = 2,
+    //% block="once in background"
+    OnceInBackground = 4,
+    //% block="forever in background"
+    ForeverInBackground = 8
+}
+
 /**
  * Pick some fruit and peel it.
  */
 //% weight=70 icon="\uf1db" color=#EC7505
 namespace musicalScore {
+    let beatsPerMinute: number = 120;
+    let freqTable: number[] = [];
+    let _playTone: (frequency: number, duration: number) => void;
+    const MICROBIT_MELODY_ID = 2000;
     /**
      * Pick a fruit
      */
@@ -71,9 +214,119 @@ namespace musicalScore {
         return "cccc"
     }
 
-        //% blockId=musicalScore_on_event block="musical event"
+    //% blockId=musicalScore_on_event block="musical event"
     //% help=music/on-event weight=59
-    export function onEvent(handler: Action) {
+    export function onEvent(handler: Action): void {
     }
 
+    //% help=music/play-tone weight=90
+    //% blockId=musicalScore_play_note block="play|tone %note=device_note|for %duration=device_beat" blockGap=8
+    //% useEnumVal=1
+    export function playTone(frequency: number, ms: number): void {
+        if (_playTone) _playTone(frequency, ms);
+        else pins.analogPitch(frequency, ms);
+    }
+
+
+    //% help=music/beat weight=49
+    //% blockId=device_beat block="%fraction|beat"
+    export function beat(fraction?: BeatFraction): number {
+        init();
+        if (fraction == null) fraction = BeatFraction.Whole;
+        let beat = 60000 / beatsPerMinute;
+        switch (fraction) {
+            case BeatFraction.Half: return beat / 2;
+            case BeatFraction.Quarter: return beat / 4;
+            case BeatFraction.Eighth: return beat / 8;
+            case BeatFraction.Sixteenth: return beat / 16;
+            case BeatFraction.Double: return beat * 2;
+            case BeatFraction.Breve: return beat * 4;
+            default: return beat;
+        }
+    }
+
+    //% help=music/set-play-tone
+    //% advanced=true
+    export function setPlayTone(f: (frequency: number, duration: number) => void) {
+        _playTone = f;
+    }
+
+    function playNextNote(melody: Melody): void {
+        // cache elements
+        let currNote = melody.nextNote();
+        let currentPos = melody.currentPos;
+        let currentDuration = melody.currentDuration;
+        let currentOctave = melody.currentOctave;
+
+        let note: number;
+        let isrest: boolean = false;
+        let beatPos: number;
+        let parsingOctave: boolean = true;
+
+        for (let pos = 0; pos < currNote.length; pos++) {
+            let noteChar = currNote.charAt(pos);
+            switch (noteChar) {
+                case 'c': case 'C': note = 1; break;
+                case 'd': case 'D': note = 3; break;
+                case 'e': case 'E': note = 5; break;
+                case 'f': case 'F': note = 6; break;
+                case 'g': case 'G': note = 8; break;
+                case 'a': case 'A': note = 10; break;
+                case 'b': case 'B': note = 12; break;
+                case 'r': case 'R': isrest = true; break;
+                case '#': note++; break;
+                case 'b': note--; break;
+                case ':': parsingOctave = false; beatPos = pos; break;
+                default: if (parsingOctave) currentOctave = parseInt(noteChar);
+            }
+        }
+        if (!parsingOctave) {
+            currentDuration = parseInt(currNote.substr(beatPos + 1, currNote.length - beatPos));
+        }
+        let beat = (60000 / beatsPerMinute) / 4;
+        if (isrest) {
+            music.rest(currentDuration * beat)
+        } else {
+            let keyNumber = note + (12 * (currentOctave - 1));
+            let frequency = keyNumber >= 0 && keyNumber < freqTable.length ? freqTable[keyNumber] : 0;
+            music.playTone(frequency, currentDuration * beat);
+        }
+        melody.currentDuration = currentDuration;
+        melody.currentOctave = currentOctave;
+        const repeating = melody.repeating && currentPos == melody.melodyArray.length - 1;
+        melody.currentPos = repeating ? 0 : currentPos + 1;
+
+        control.raiseEvent(MICROBIT_MELODY_ID, melody.background ? MusicEvent.BackgroundMelodyNotePlayed : MusicEvent.MelodyNotePlayed);
+        if (repeating)
+            control.raiseEvent(MICROBIT_MELODY_ID, melody.background ? MusicEvent.BackgroundMelodyRepeated : MusicEvent.MelodyRepeated);
+    }
+
+    class Melody {
+        public melodyArray: string[];
+        public currentDuration: number;
+        public currentOctave: number;
+        public currentPos: number;
+        public repeating: boolean;
+        public background: boolean;
+
+        constructor(melodyArray: string[], options: MelodyOptions) {
+            this.melodyArray = melodyArray;
+            this.repeating = ((options & MelodyOptions.Forever) != 0);
+            this.repeating = this.repeating ? true : ((options & MelodyOptions.ForeverInBackground) != 0)
+            this.background = ((options & MelodyOptions.OnceInBackground) != 0);
+            this.background = this.background ? true : ((options & MelodyOptions.ForeverInBackground) != 0);
+            this.currentDuration = 4; //Default duration (Crotchet)
+            this.currentOctave = 4; //Middle octave
+            this.currentPos = 0;
+        }
+
+        hasNextNote() {
+            return this.repeating || this.currentPos < this.melodyArray.length;
+        }
+
+        nextNote(): string {
+            const currentNote = this.melodyArray[this.currentPos];
+            return currentNote;
+        }
+    }
 }
