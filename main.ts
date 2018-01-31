0b117c0493bf24527871026b61e75233ba0880df
isnkng <35251708+isnkng@users.noreply.github.com>
2018-01-31 17:32:21 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,73 +1,145 @@
 /**
- * Types of musicalScore
+ * Types of music
  */
-enum TropicalFruit {
-    //% block=banana
-    Banana = 0,
-    //% block=pineapple
-    Pinapple = 1,
-    //% block=coconut
-    Coconut = 2
+enum Note {
+    //% blockIdentity=music.noteFrequency enumval=262
+    C = 262,
+    //% block=C#
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F = 349,
+    //% block=F#
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G = 392,
+    //% block=G#
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B = 494,
+    //% blockIdentity=music.noteFrequency enumval=131
+    C3 = 131,
+    //% block=C#3
+    //% blockIdentity=music.noteFrequency enumval=139
+    CSharp3 = 139,
+    //% blockIdentity=music.noteFrequency enumval=147
+    D3 = 147,
+    //% blockIdentity=music.noteFrequency enumval=156
+    Eb3 = 156,
+    //% blockIdentity=music.noteFrequency enumval=165
+    E3 = 165,
+    //% blockIdentity=music.noteFrequency enumval=175
+    F3 = 175,
+    //% block=F#3
+    //% blockIdentity=music.noteFrequency enumval=185
+    FSharp3 = 185,
+    //% blockIdentity=music.noteFrequency enumval=196
+    G3 = 196,
+    //% block=G#3
+    //% blockIdentity=music.noteFrequency enumval=208
+    GSharp3 = 208,
+    //% blockIdentity=music.noteFrequency enumval=220
+    A3 = 220,
+    //% blockIdentity=music.noteFrequency enumval=233
+    Bb3 = 233,
+    //% blockIdentity=music.noteFrequency enumval=247
+    B3 = 247,
+    //% blockIdentity=music.noteFrequency enumval=262
+    C4 = 262,
+    //% block=C#4
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp4 = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D4 = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb4 = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E4 = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F4 = 349,
+    //% block=F#4
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp4 = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G4 = 392,
+    //% block=G#4
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp4 = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A4 = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb4 = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B4 = 494,
+    //% blockIdentity=music.noteFrequency enumval=523
+    C5 = 523,
+    //% block=C#5
+    //% blockIdentity=music.noteFrequency enumval=555
+    CSharp5 = 555,
+    //% blockIdentity=music.noteFrequency enumval=587
+    D5 = 587,
+    //% blockIdentity=music.noteFrequency enumval=622
+    Eb5 = 622,
+    //% blockIdentity=music.noteFrequency enumval=659
+    E5 = 659,
+    //% blockIdentity=music.noteFrequency enumval=698
+    F5 = 698,
+    //% block=F#5
+    //% blockIdentity=music.noteFrequency enumval=740
+    FSharp5 = 740,
+    //% blockIdentity=music.noteFrequency enumval=784
+    G5 = 784,
+    //% block=G#5
+    //% blockIdentity=music.noteFrequency enumval=831
+    GSharp5 = 831,
+    //% blockIdentity=music.noteFrequency enumval=880
+    A5 = 880,
+    //% blockIdentity=music.noteFrequency enumval=932
+    Bb5 = 932,
+    //% blockIdentity=music.noteFrequency enumval=988
+    B5 = 988,
 }
 
-/**
- * Pick some fruit and peel it.
- */
+enum BeatFraction {
+    //% block=1
+    Whole = 1,
+    //% block="1/2"
+    Half = 2,
+    //% block="1/4"
+    Quarter = 4,
+    //% block="1/8"
+    Eighth = 8,
+    //% block="1/16"
+    Sixteenth = 16,
+    //% block="2"
+    Double = 32,
+    //% block="4",
+    Breve = 64,
+    //% block="3",
+    DottedQuarter = 128
+}
+
 //% weight=70 icon="\uf1db" color=#EC7505
-namespace musicalScore {
-    let beatsPerMinute: number = 120;
-    let freqTable: number[] = [];
-    let _playTone: (frequency: number, duration: number) => void;
-    const MICROBIT_MELODY_ID = 2000;
-    /**
-     * Pick a fruit
-     */
-    // //% blockId=tropic_pick block="pick a %fruit"
-    // export function pick(fruit: TropicalFruit): boolean {
-    //     return true;
-    // }
-    // /**
-    //  * Peel the fruit if possible
-    //  */
-    // //% blockId=tropic_peel block="peel a %fruit"
-    // export function peel(fruit: TropicalFruit): boolean {
-    //     return (fruit == TropicalFruit.Banana);
-    // }
-    
-    // //% blockId=musicalScore_unko block="unko a %fruit"
-    // export function unko(fruit: TropicalFruit): boolean{
-    //     return true;
-    // }
-
-    // //% blockId=musicalScore_show block="show a %fruit"
-    // export function show(fruit: TropicalFruit): void{
-
-    // }
-
-    // //% blockId=musicalScore_aaaa block="aaaa"
-    // export function aaaa(): String{
-    //     return "aaaa"
-    // }
-
-
-    // //% blockId=musicalScore_bbbb block="bbbb"
-    // export function bbbb() {
-        
-    // }
-
-    // //% blockId=melody_on_event block="test"
-    // export function test() {
-
-    // }
+namespace music {
 
     //% blockId=musicalScore_bbbb block="bbbb"
     export function bbbb() {
-        
     }
 
     //% blockId=musicalScore_aaaa block="aaaa"
     export function aaaa(): void{
-        
     }
 
     //% blockId=musicalScore_cccc block="cccc"
@@ -76,9 +148,98 @@ namespace musicalScore {
     }
 
         //% blockId=musicalScore_on_event block="musical event"
-    //% help=music/on-event weight=59
+    //% help=music/on-event
     export function onEvent(handler: Action): void {
     }
-    
+    let beatsPerMinute: number = 120;
+    let freqTable: number[] = [];
+    let _playTone: (frequency: number, duration: number) => void;
+    const MICROBIT_MELODY_ID = 2000;
 
+    /**
+     * Plays a tone through pin ``P0`` for the given duration.
+     * @param frequency pitch of the tone to play in Hertz (Hz)
+     * @param ms tone duration in milliseconds (ms)
+     */
+    //% help=music/play-tone weight=90
+    //% blockId=device_play_note block="play|tone %note=device_note|for %duration=device_beat" blockGap=8
+    //% parts="headphone"
+    //% useEnumVal=1
+    export function playTone(frequency: number, ms: number): void {
+        if (_playTone) _playTone(frequency, ms);
+        else pins.analogPitch(frequency, ms);
+    }
+
+    /**
+     * Plays a tone through pin ``P0``.
+     * @param frequency pitch of the tone to play in Hertz (Hz)
+     */
+    //% help=music/ring-tone weight=80
+    //% blockId=device_ring block="ring tone (Hz)|%note=device_note" blockGap=8
+    //% parts="headphone"
+    //% useEnumVal=1
+    export function ringTone(frequency: number): void {
+        playTone(frequency, 0);
+    }
+
+    /**
+     * Rests (plays nothing) for a specified time through pin ``P0``.
+     * @param ms rest duration in milliseconds (ms)
+     */
+    //% help=music/rest weight=79
+    //% blockId=device_rest block="rest(ms)|%duration=device_beat"
+    //% parts="headphone"
+    export function rest(ms: number): void {
+        playTone(0, ms);
+    }
+
+
+    /**
+     * Gets the frequency of a note.
+     * @param name the note name, eg: Note.C
+     */
+    //% weight=50 help=music/note-frequency
+    //% blockId=device_note block="%note"
+    //% shim=TD_ID
+    //% note.fieldEditor="note" note.defl="262"
+    //% useEnumVal=1
+    export function noteFrequency(name: Note): number {
+        return name;
+    }
+
+    function init() {
+        if (beatsPerMinute <= 0) beatsPerMinute = 120;
+        if (freqTable.length == 0) freqTable = [31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186]
+    }
+
+    /**
+     * Returns the duration of a beat in milli-seconds
+     */
+    //% help=music/beat weight=49
+    //% blockId=device_beat block="%fraction|beat"
+    export function beat(fraction?: BeatFraction): number {
+        init();
+        if (fraction == null) fraction = BeatFraction.Whole;
+        let beat = 60000 / beatsPerMinute;
+        switch (fraction) {
+            case BeatFraction.Half: return beat / 2;
+            case BeatFraction.Quarter: return beat / 4;
+            case BeatFraction.Eighth: return beat / 8;
+            case BeatFraction.Sixteenth: return beat / 16;
+            case BeatFraction.Double: return beat * 2;
+            case BeatFraction.Breve: return beat * 4;
+            case BeatFraction.DottedQuarter: return beat * 3;
+            default: return beat;
+        }
+    }
+
+    /**
+     * Returns the tempo in beats per minute. Tempo is the speed (bpm = beats per minute) at which notes play. The larger the tempo value, the faster the notes will play.
+     */
+    //% help=music/tempo weight=40
+    //% blockId=device_tempo block="tempo (bpm)" blockGap=8
+    export function tempo(): number {
+        init();
+        return beatsPerMinute;
+    }
 }
