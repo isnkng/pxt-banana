173a7ce5dc76c59f25f08f1e4d24a5cc63075380
isnkng <35251708+isnkng@users.noreply.github.com>
2018-02-09 13:21:17 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,118 +1,5 @@
-/**
- * Types of music
- */
-enum Note {
-    //% blockIdentity=music.noteFrequency enumval=262
-    C = 262,
-    //% block=C#
-    //% blockIdentity=music.noteFrequency enumval=277
-    CSharp = 277,
-    //% blockIdentity=music.noteFrequency enumval=294
-    D = 294,
-    //% blockIdentity=music.noteFrequency enumval=311
-    Eb = 311,
-    //% blockIdentity=music.noteFrequency enumval=330
-    E = 330,
-    //% blockIdentity=music.noteFrequency enumval=349
-    F = 349,
-    //% block=F#
-    //% blockIdentity=music.noteFrequency enumval=370
-    FSharp = 370,
-    //% blockIdentity=music.noteFrequency enumval=392
-    G = 392,
-    //% block=G#
-    //% blockIdentity=music.noteFrequency enumval=415
-    GSharp = 415,
-    //% blockIdentity=music.noteFrequency enumval=440
-    A = 440,
-    //% blockIdentity=music.noteFrequency enumval=466
-    Bb = 466,
-    //% blockIdentity=music.noteFrequency enumval=494
-    B = 494,
-    //% blockIdentity=music.noteFrequency enumval=131
-    C3 = 131,
-    //% block=C#3
-    //% blockIdentity=music.noteFrequency enumval=139
-    CSharp3 = 139,
-    //% blockIdentity=music.noteFrequency enumval=147
-    D3 = 147,
-    //% blockIdentity=music.noteFrequency enumval=156
-    Eb3 = 156,
-    //% blockIdentity=music.noteFrequency enumval=165
-    E3 = 165,
-    //% blockIdentity=music.noteFrequency enumval=175
-    F3 = 175,
-    //% block=F#3
-    //% blockIdentity=music.noteFrequency enumval=185
-    FSharp3 = 185,
-    //% blockIdentity=music.noteFrequency enumval=196
-    G3 = 196,
-    //% block=G#3
-    //% blockIdentity=music.noteFrequency enumval=208
-    GSharp3 = 208,
-    //% blockIdentity=music.noteFrequency enumval=220
-    A3 = 220,
-    //% blockIdentity=music.noteFrequency enumval=233
-    Bb3 = 233,
-    //% blockIdentity=music.noteFrequency enumval=247
-    B3 = 247,
-    //% blockIdentity=music.noteFrequency enumval=262
-    C4 = 262,
-    //% block=C#4
-    //% blockIdentity=music.noteFrequency enumval=277
-    CSharp4 = 277,
-    //% blockIdentity=music.noteFrequency enumval=294
-    D4 = 294,
-    //% blockIdentity=music.noteFrequency enumval=311
-    Eb4 = 311,
-    //% blockIdentity=music.noteFrequency enumval=330
-    E4 = 330,
-    //% blockIdentity=music.noteFrequency enumval=349
-    F4 = 349,
-    //% block=F#4
-    //% blockIdentity=music.noteFrequency enumval=370
-    FSharp4 = 370,
-    //% blockIdentity=music.noteFrequency enumval=392
-    G4 = 392,
-    //% block=G#4
-    //% blockIdentity=music.noteFrequency enumval=415
-    GSharp4 = 415,
-    //% blockIdentity=music.noteFrequency enumval=440
-    A4 = 440,
-    //% blockIdentity=music.noteFrequency enumval=466
-    Bb4 = 466,
-    //% blockIdentity=music.noteFrequency enumval=494
-    B4 = 494,
-    //% blockIdentity=music.noteFrequency enumval=523
-    C5 = 523,
-    //% block=C#5
-    //% blockIdentity=music.noteFrequency enumval=555
-    CSharp5 = 555,
-    //% blockIdentity=music.noteFrequency enumval=587
-    D5 = 587,
-    //% blockIdentity=music.noteFrequency enumval=622
-    Eb5 = 622,
-    //% blockIdentity=music.noteFrequency enumval=659
-    E5 = 659,
-    //% blockIdentity=music.noteFrequency enumval=698
-    F5 = 698,
-    //% block=F#5
-    //% blockIdentity=music.noteFrequency enumval=740
-    FSharp5 = 740,
-    //% blockIdentity=music.noteFrequency enumval=784
-    G5 = 784,
-    //% block=G#5
-    //% blockIdentity=music.noteFrequency enumval=831
-    GSharp5 = 831,
-    //% blockIdentity=music.noteFrequency enumval=880
-    A5 = 880,
-    //% blockIdentity=music.noteFrequency enumval=932
-    Bb5 = 932,
-    //% blockIdentity=music.noteFrequency enumval=988
-    B5 = 988,
-}
 
-enum BeatFraction {
+enum Beat {
     //% block=1
     Whole = 1,
     //% block="1/2"
@@ -128,17 +15,17 @@ enum BeatFraction {
     //% block="4",
     Breve = 64,
     //% block="3"
-    DottedHalf = 3,
+    DottedHalf = 128,
     //% block="3/2"
-    DottedQuarter = 32,
+    DottedQuarter = 256,
     //% block="3/4"
-    DottedEighth = 34,
+    DottedEighth = 512,
     //% block="3/8" 
-    DottedSixteenth = 38
+    DottedSixteenth = 1024
 }
 
 //% color=#D83B01 weight=98 icon="\uf025"
-namespace music {
+namespace beats {
     let beatsPerMinute: number = 120;
     let freqTable: number[] = [];
     let _playTone: (frequency: number, duration: number) => void;
@@ -177,21 +64,21 @@ namespace music {
      */
     //% help=music/beat weight=49
     //% blockId=device_beat block="%fraction|beat"
-    export function beat(fraction?: BeatFraction): number {
+    export function notesbeat(fraction?: Beat): number {
         init();
-        if (fraction == null) fraction = BeatFraction.Whole;
+        if (fraction == null) fraction = Beat.Whole;
         let beat = 60000 / beatsPerMinute;
         switch (fraction) {
-            case BeatFraction.Half: return beat / 2;
-            case BeatFraction.Quarter: return beat / 4;
-            case BeatFraction.Eighth: return beat / 8;
-            case BeatFraction.Sixteenth: return beat / 16;
-            case BeatFraction.Double: return beat * 2;
-            case BeatFraction.Breve: return beat * 4;
-            case BeatFraction.DottedHalf: return beat * 3;
-            case BeatFraction.DottedQuarter: return beat * 3/2;
-            case BeatFraction.DottedEighth: return beat * 3/4;
-            case BeatFraction.DottedSixteenth: return beat * 3/8;
+            case Beat.Half: return beat / 2;
+            case Beat.Quarter: return beat / 4;
+            case Beat.Eighth: return beat / 8;
+            case Beat.Sixteenth: return beat / 16;
+            case Beat.Double: return beat * 2;
+            case Beat.Breve: return beat * 4;
+            case Beat.DottedHalf: return beat * 3;
+            case Beat.DottedQuarter: return beat * 3/2;
+            case Beat.DottedEighth: return beat * 3/4;
+            case Beat.DottedSixteenth: return beat * 3/8;
             default: return beat;
         }
     }
