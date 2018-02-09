b7b9679a71a6f3df381a699affbcd3e6fe719810
isnkng <35251708+isnkng@users.noreply.github.com>
2018-02-09 18:00:50 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -31,13 +31,25 @@ namespace beats {
     let _playTone: (frequency: number, duration: number) => void;
     const MICROBIT_MELODY_ID = 2000;
 
+    function init() {
+        if (beatsPerMinute <= 0) beatsPerMinute = 120;
+        if (freqTable.length == 0) freqTable = [31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186]
+    }
+
+    // //% blockId=beats_TrebleClef block="ト音記号 BPM|%value"
+    // //% bpm.min=4 bpm.max=400
+    // export function TrebleClef(): void {
+    //     init();
+    //     if (bpm > 0) {
+    //         beatsPerMinute = Math.max(1, bpm);
+    //     }
+    //     setTempo(bpm);
+    // }
+
     //% blockId=beats_TrebleClef block="ト音記号 BPM|%value"
     //% bpm.min=4 bpm.max=400
-    export function TrebleClef(bpm: number): void {
-        init();
-        if (bpm > 0) {
-            beatsPerMinute = Math.max(1, bpm);
-        }
+    export function TrebleClef(body: Action): void {
+
     }
 
     //% blockId=beats_BassClef block="ヘ音記号 BPM|%value"
@@ -47,6 +59,7 @@ namespace beats {
         if (bpm > 0) {
             beatsPerMinute = Math.max(1, bpm);
         }
+        setTempo(bpm);
     }
 
     //% blockId=musicalScore_cccc block="cccc"
@@ -59,10 +72,7 @@ namespace beats {
     // export function onEvent(handler: Action): void {
     // }
 
-    function init() {
-        if (beatsPerMinute <= 0) beatsPerMinute = 120;
-        if (freqTable.length == 0) freqTable = [31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186]
-    }
+
 
     /**
      * Returns the duration of a beat in milli-seconds
